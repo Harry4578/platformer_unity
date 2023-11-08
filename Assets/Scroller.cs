0c34bac2ee15de9ff38790dd55c585762d12d101
Harry4578 <23686917@stu.mmu.ac.uk>
2023-11-08 12:26:26 +0000

    scroller sort of working

--- Assets/Scroller.cs
+++ Assets/Scroller.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Scroller : MonoBehaviour
 {
+    int numLayers;
+    Transform[] layers;
+    public float[] layerFac;
+    public Camera attachedCamera;
+    Vector3 initialPosition;
     // Start is called before the first frame update
     void Start()
     {
-        
+        numLayers = layerFac.Length;
+        layers = new Transform[numLayers];
+        initialPosition = attachedCamera.transform.position;
+
+        for (int i = 0; i < numLayers; i++)
+        {
+            layers[i] = transform.GetChild(i);
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 diff = attachedCamera.transform.position - initialPosition;
+        for (int i = 0; i < numLayers; i++)
+        {
+            Vector3 scaledDiff = diff * layerFac[i];
+            layers[i].transform.localPosition = new Vector3(scaledDiff.x,
+           scaledDiff.y, 0.0f);
+        }
     }
+
 }
+
